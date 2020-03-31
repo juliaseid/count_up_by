549f8808d58c4745e6c861e0a92393206dbb0a77
unknown <macjay314@gmail.com>
2020-03-31 10:24:05 -0700

    create two functions that count to the final number (depending upon if it's negative)

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,35 @@
+function countUpPos(num1, num2) {
+  for (var i=0; i<=num1; i+=num2) {
+    $(".output").text(i);
+  }
+}
+
+function countDownNeg(num1, num2) {
+  for (var i=0; i>=num1; i+=num2) {
+    $(".output").text(i);
+  }
+}
+
+
 $(document).ready(function(){
   $(".form").submit(function(){
     var num1 = parseInt($("input#num1").val());
     var num2 = parseInt($("input#num2").val());
-    if(num1 || num2 === null){
+    if(num1 === ""|| num2 === ""){
       $("#empty-value").show();
     } else if (isNaN(num1) || isNaN(num2)){
       $("#Nan-value").show();
-    } else if (num1 < 0 || num2 < 0){
+    } else if ((num1 < 0 && num2 > 0) || (num1 > 0 && num2 < 0)){
       $("#negative-value").show();      
-    } else {
+    } else if (Math.abs(num1) < Math.abs(num2)){ 
       $("#larger-than-value").show();
+    } else if (num1 < 0){
+      countDownNeg(num1, num2);
     }
-
+    else{
+      countUpPos(num1, num2)
+    }
+    event.preventDefault();
 
 
   })
